e9c74c523fd7f8f5f2d995ed1250ffdc95e570b4
bluesimp1102 <jackismyname7@gmail.com>
2024-04-29 21:59:44 -0700

    Add linting for commit messages.

--- app/(inner-layout)/development/page.tsx
+++ app/(inner-layout)/development/page.tsx
@@ -3,8 +3,14 @@ import React from "react";
 
 const DevelopmentPage = (): JSX.Element => {
   return (
-    <div className="w-full max-w-5xl mx-auto mt-2">
+    <div className="w-full max-w-5xl mx-auto mt-2 flex flex-col gap-4 px-4">
       <ToastersPlaygroud />
+      <div className="bg-secondary text-secondary-foreground p-4 rounded-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+        delectus recusandae. Distinctio rerum earum numquam debitis itaque cum
+        quos quibusdam dolores hic, sapiente excepturi voluptate nihil fuga
+        corrupti ullam nobis?
+      </div>
     </div>
   );
 };
--- components/dev/toasters.tsx
+++ components/dev/toasters.tsx
@@ -39,40 +39,38 @@ const ToastersPlaygroud = (): JSX.Element => {
   const [toastType, setToastType] = React.useState<ToastTypes>("error");
 
   return (
-    <div className="px-4">
-      <div className="flex flex-col gap-2">
-        <Label>Toast Playground</Label>
-        <div className="flex items-center gap-2">
-          <Select
-            defaultValue={toastType}
-            onValueChange={(value) => {
-              setToastType(value as ToastTypes);
-            }}
-          >
-            <SelectTrigger className="min-w-[180px] w-fit">
-              <SelectValue placeholder="toaster type" />
-            </SelectTrigger>
-            <SelectContent>
-              {toastTypes.map((item, index) => (
-                <SelectItem key={index} value={item}>
-                  {item}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <Button
-            variant={toastType}
-            onClick={() => {
-              if (toastType === "default") {
-                toast(toastContent.title, toastContent.content);
-              } else {
-                toast[toastType](toastContent.title, toastContent.content);
-              }
-            }}
-          >
-            Toast!
-          </Button>
-        </div>
+    <div className="flex flex-col gap-2">
+      <Label>Toast Playground</Label>
+      <div className="flex items-center gap-2">
+        <Select
+          defaultValue={toastType}
+          onValueChange={(value) => {
+            setToastType(value as ToastTypes);
+          }}
+        >
+          <SelectTrigger className="min-w-[180px] w-fit">
+            <SelectValue placeholder="toaster type" />
+          </SelectTrigger>
+          <SelectContent>
+            {toastTypes.map((item, index) => (
+              <SelectItem key={index} value={item}>
+                {item}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Button
+          variant={toastType}
+          onClick={() => {
+            if (toastType === "default") {
+              toast(toastContent.title, toastContent.content);
+            } else {
+              toast[toastType](toastContent.title, toastContent.content);
+            }
+          }}
+        >
+          Toast!
+        </Button>
       </div>
     </div>
   );
