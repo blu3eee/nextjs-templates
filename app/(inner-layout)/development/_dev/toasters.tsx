dfbdfe41d3c6507f042b67c7eae09332b3bfe665
bluesimp1102 <jackismyname7@gmail.com>
2024-04-30 09:43:33 -0700

    refactor: refactor dev components

---
+++ app/(inner-layout)/development/_dev/toasters.tsx
@@ -0,0 +1,120 @@
+"use client";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import React from "react";
+import { Button } from "../../../../components/ui/button";
+import { toast } from "sonner";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { poppinsFont } from "@/styles/fonts";
+import { Loader2 } from "lucide-react";
+
+const toastTypes = ["default", "success", "info", "warning", "error"] as const;
+
+// Derive the union type from the array
+type ToastTypes = (typeof toastTypes)[number];
+
+const ToastersPlayground = (): JSX.Element => {
+  const toastContent = {
+    title: "Event has been created",
+    content: {
+      description: "Sunday, December 03, 2023 at 9:00 AM",
+      action: {
+        label: "Undo",
+        onClick: () => {
+          console.log("Undo");
+        },
+      },
+      cancel: {
+        label: "Cancel",
+        onClick: () => {
+          console.log("Cancel!");
+        },
+      },
+    },
+  };
+
+  const [toastType, setToastType] = React.useState<ToastTypes>("error");
+
+  const myPromiseFn = async (time?: number): Promise<{ name: string }> => {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ name: "Custom loader" });
+      }, time ?? 3000);
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className={cn("text-lg font-semibold", poppinsFont.className)}>
+        Toast Playground
+      </div>
+      <div className="flex flex-wrap gap-4">
+        <div className="flex flex-col gap-2">
+          <Label className="text-sm text-foreground/80 font-medium">
+            toast type
+          </Label>
+          <Select
+            defaultValue={toastType}
+            onValueChange={(value) => {
+              setToastType(value as ToastTypes);
+            }}
+          >
+            <SelectTrigger className="min-w-[180px] w-fit">
+              <SelectValue placeholder="toaster type" />
+            </SelectTrigger>
+            <SelectContent>
+              {toastTypes.map((item, index) => (
+                <SelectItem key={index} value={item}>
+                  {item}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <Button
+            variant={toastType}
+            onClick={() => {
+              if (toastType === "default") {
+                toast(toastContent.title, toastContent.content);
+              } else {
+                toast[toastType](toastContent.title, toastContent.content);
+              }
+            }}
+            className="w-fit"
+          >
+            Toast!
+          </Button>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label className="text-sm text-foreground/80 font-medium">
+            toast loading
+          </Label>
+
+          <Button
+            variant={"secondary"}
+            onClick={() => {
+              toast.promise(myPromiseFn, {
+                loading: "Loading promise...",
+                success: (data) => {
+                  return `${data.name} toast has been added`;
+                },
+                error: "Error",
+              });
+            }}
+            className="w-fit gap-2"
+          >
+            <Loader2 className="animate-spin" />
+            Toast with loading!
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ToastersPlayground;
--- app/(inner-layout)/development/page.tsx
+++ app/(inner-layout)/development/page.tsx
@@ -1,15 +1,21 @@
-import ToastersPlaygroud from "@/components/dev/toasters";
+import ToastersPlayground from "@/app/(inner-layout)/development/_dev/toasters";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const DevelopmentPage = (): JSX.Element => {
   return (
     <div className="w-full max-w-5xl mx-auto mt-2 flex flex-col gap-4 px-4">
-      <ToastersPlaygroud />
-      <div className="bg-secondary text-secondary-foreground p-4 rounded-lg">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-        delectus recusandae. Distinctio rerum earum numquam debitis itaque cum
-        quos quibusdam dolores hic, sapiente excepturi voluptate nihil fuga
-        corrupti ullam nobis?
+      <ToastersPlayground />
+      <div className="bg-secondary text-secondary-foreground p-4 rounded-lg flex flex-col gap-2">
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
+          delectus recusandae. Distinctio rerum earum numquam debitis itaque cum
+          quos quibusdam dolores hic, sapiente excepturi voluptate nihil fuga
+          corrupti ullam nobis?
+        </span>
+        <Button disabled className="w-fit self-end" size={"sm"}>
+          Lorem
+        </Button>
       </div>
     </div>
   );
